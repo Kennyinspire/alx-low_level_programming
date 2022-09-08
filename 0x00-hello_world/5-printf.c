b592b2e417ed7d776086469eedb4217f0c66b0c5
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-08 16:58:54 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that prints with printf function.
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
