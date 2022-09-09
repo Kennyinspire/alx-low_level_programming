e7bbd24de160dee99954fb804ee39c14eddacba5
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-08 17:28:01 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - entry point.
+ * C program that compile without any warnings
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
