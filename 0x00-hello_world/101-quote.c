efbaf11f2227a19535cfd5dc81f8cf0a34cf932e
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-08 17:48:19 -0700

    Fix quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,9 @@
  * main - entry point.
  * Description:  C program that compile without any warnings
  */
+/**
+ * Description:  C program that compile without any warnings
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
