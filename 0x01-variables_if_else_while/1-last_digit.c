f6d2719f74bfd8101f3615576ea8c41e75753631
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-09 17:43:40 -0700

    fix lint error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,32 +6,30 @@
 
 /**
  * main- entry point
- * Description: A script that print variable n and determines
- * if it is negative or positive
+ * Description: A script that print variable n and it's last digit
  * Return: return 0 as success
  */
 
 int main(void)
 {
-  int n, lastDigit;
-
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-
-  lastDigit = n % 10;
-
-  if (lastDigit > 5)
-  {
-    printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-  }
-  else if (lastDigit == 0)
-  {
-    printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-  }
-  else
-  {
-    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-  }
-
-  return (0);
+	int n, lastDigit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	lastDigit = n % 10;
+	
+	if (lastDigit > 5)
+ 	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+	
+	return (0);
 }
