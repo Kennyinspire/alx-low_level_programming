025c40bf1da1ad8cd92590e1df474e805fbe6dea
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-09 19:52:28 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A program that prints all possible
+ * different combinations of two digits
+ * Return: returns 0
+*/
+
+int main(void)
+{
+	int a = 0, b;
+
+	while (a <= 9)
+	{
+		b = a + 1;
+
+		while (b <= 9)
+		{
+
+			putchar(a + 48);
+			putchar(b + 48);
+
+			if (a == 8 && b == 9)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++
+	}
+	return (0);
+}
