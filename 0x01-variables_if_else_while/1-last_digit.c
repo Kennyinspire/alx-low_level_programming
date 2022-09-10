d36662c7f4ab870e66b836f459db98654348df9f
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-09 17:36:42 -0700

    Hotfix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
 /**
@@ -7,29 +9,29 @@
  * Description: A script that print variable n and determines
  * if it is negative or positive
  * Return: return 0 as success
-*/
+ */
 
 int main(void)
 {
-        int n, lastDigit;
+  int n, lastDigit;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10;
+  lastDigit = n % 10;
 
-        if (lastDigit > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-        }
-        else if (lastDigit == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-        }
-        else
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-        }
+  if (lastDigit > 5)
+  {
+    printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+  }
+  else if (lastDigit == 0)
+  {
+    printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+  }
+  else
+  {
+    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+  }
 
-        return (0);
+  return (0);
 }
