adaa4791a328a57f3ab481a2b30f362d25d2779c
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-14 14:21:03 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function to add 2 integers
+ * @num1: first input
+ * @num2: second input
+ * Return: returns the sum
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
