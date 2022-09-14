9334a4ebcc00bc7d1b489604754455e53734518f
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-13 20:43:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Description: A program that prints _putchar
+ * Return: return 0 as success
+*/
+
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
