f715cd01b0d2ccb95c3b1944f89e10572c5b655e
Adeyemi <adunola.adeyemi@gmail.com>
2022-09-15 13:02:43 -0700

    quick fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -18,7 +17,7 @@ i = 0;
 *putchar(i);
 *}
 */
-printf("Infinte loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
