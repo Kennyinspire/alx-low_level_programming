7c56d6ff864f460a0bf5111afb0258fb926797d9
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-16 15:54:32 -0700

    multiple numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples of two numbers
+ * @a: first number
+ * @b: secon number
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	return (a + b);
+}
