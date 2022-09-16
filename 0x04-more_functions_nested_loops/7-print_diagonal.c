5530ff5f546b2cac05e8c0cb321d95ebcdc726a3
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-16 16:52:22 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  print_diagonal - A program that prints diagonal
+ *  @n: parameter
+ *  Retrun: return nothing
+ */
+
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+			{
+				putchar(' ');
+			}
+
+			putchar('\\');
+
+			if (len == (n - 1))
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
