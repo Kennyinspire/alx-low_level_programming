1a2fc014d73ba2745f1f685d7df475335749fc69
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-16 17:00:58 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ * @size: parameter
+ * Return: return nothing
+ */
+
+void print_square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 0; inc1 < size; inc++)
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			{
+				putchar('#');
+				putchar('\n');
+			}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
