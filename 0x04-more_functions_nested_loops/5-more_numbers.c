cdabf3c81402752a41214fbd5eb3e07803cb58d4
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-16 17:35:03 -0700

    last fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,13 @@ void more_numbers(void)
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 <= 14; n2++)
+		{
 			if (n2 > 9)
 			{
 				putchar((n2 / 10) + '0');
 			}
-		putchar((n2 % 10) + '0');
-	}
+			putchar((n2 % 10) + '0');
+		}
 		putchar(10);
 	}
 }
