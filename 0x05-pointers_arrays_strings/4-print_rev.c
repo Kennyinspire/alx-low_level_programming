038dd506a337c8268dcaa08d3113fb2321989ab6
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-19 14:47:25 -0700

    print in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev- A program that prints a string in reverse order
+ * @s: string to reverse
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
