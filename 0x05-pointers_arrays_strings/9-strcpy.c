bad3d9ffa7760f18a67321a4cb5c16937e746735
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-19 15:33:50 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * _strcpy - copy and pase string
+ * @dst:  destination
+ * @src:  source
+ *
+ * Return: destination
+ */
+
+char *_strcpy(char *dst, char *src)
+{
+	int inc = 0;
+
+	while (*(src + inc) != '\0')
+	{
+		*(dst + inc) = *(src + inc);
+		inc++;
+	}
+	*(dst + inc) = '\0';
+
+	return (dst);
+}
