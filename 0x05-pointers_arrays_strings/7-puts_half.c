a84df18f71f5c9d0cf0683d98ca6a61f5b71cd1e
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-20 13:33:20 -0700

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strlen - A program to get length of string
+ * @s: pointer to char
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * puts_half - prints half of a string
+ * @str: a pointer to string
+ * Return: nothing
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int size = _strlen(str);
+
+	if (size % 2 != 0)
+	{
+		i = (size / 2) + 1;
+	}
+	else
+	{
+		i = size / 2;
+	}
+
+	while (i < size)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
