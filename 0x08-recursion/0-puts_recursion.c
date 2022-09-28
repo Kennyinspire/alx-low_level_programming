4f0e71dfd6a1d456d4454809471b8a6e65821c3e
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-09-28 13:11:23 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string follow by a new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
