a1ef490949133ddabb3fcf23f86e6763fbcaf683
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-04 01:00:07 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,32 +2,34 @@
 
 /**
  * str_concat - A function that concatenates two string.
- * @str1:First string
- * @str2:Second string
+ * @s1:First string
+ * @s2:Second string
  *
  * Return: NULL if the function fails,
  * pointer to new string in case of success
  */
 
-char *str_concat(char *str1, char *str2)
+char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 	int index, concat_index = 0, len = 0;
 
-	if (str1 == NULL)
-		str1 = "";
-	if (str2 == NULL)
-		str2 "";
-	for (index = 0; str1[index] || str2[index]; index++)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 "";
+	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
 	concat_str = malloc(sizeof(char) * len);
 
 	if (concat_str == NULL)
 		return (NULL);
-	for (index = 0; str1[index]; index++)
-		concat_str[concat_index++] = str1[index];
-	for (index = 0; str2[index]; index++)
-		concat_str[concat_index++] = str2[index];
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
 	return (concat_str);
 }
