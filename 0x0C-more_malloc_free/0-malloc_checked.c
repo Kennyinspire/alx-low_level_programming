a02de89602679c0793052cd0a33010220c7d96b7
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-07 11:31:33 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * malloc_checked - array for printing a string
+ * malloc_checked - array for prints a string
  * @b: number of memory
  *
  * Return: void
@@ -9,13 +9,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-void *p;
+	void *p;
 
-p = malloc(b);
-if (p == NULL)
-{
-exit(98);
-}
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
 
-return (p);
+	return (p);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,4 +12,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* MAIN_H */
+#endif
