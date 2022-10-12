5b789cfb9563074c68558153d0e6b5090bb91073
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-12 14:48:01 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-cal.h"
+
+/**
+ * get_op_func - Function pointer thet selects the correct function
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
