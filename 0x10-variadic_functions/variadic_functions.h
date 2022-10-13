da555c82e24a28a5380d1413f654372040759da3
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-13 13:59:08 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,15 +4,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list arg);
-
-}
-printer_t;
-
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
