ecfadad5634d86fee016fa9c28af397ca1a03f24
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-13 13:57:11 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,32 +1,29 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - ...
- * @n: ...
+ * sum_them_all - Return the sum of all its parameters
+ * @n: argument counts
  *
- * Return: ...
+ * Return: sum of its parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list arg;
-	unsigned int i = 0;
+
+	va_list valist;
+	unsigned int i;
 	int sum = 0;
 
-	if (n != 0)
-	{
-		va_start(args, n);
+	if (n == 0)
+		return (0);
 
-		while (i < n)
-		{
-			sum += va_arg(args, int);
+	va_start(valist, n);
 
-			i++;
-		}
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
 
-		va_end(args);
-		return (sum);
-	}
+	va_end(valist);
 
-	return (0);
+	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
 
 typedef struct printer
 {
