b58ee02749f78f7730685f74e7e9d65ace96a5ca
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-13 15:00:23 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,30 +25,30 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == t_arg[j] && c)
 			{
-				printf(", ")
+				printf(", ");
 					break;
 			} j++;
 		}
 		switch (format[i])
 		{
-			case "c":
-				printf("%c", va_arg(valist, int)), c = 1;
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
 				break;
-			case "i":
-				printf("%d", va_arg(valist, int)), c = 1;
-				break;
-			case "f":
-				printf("%f", va_arg(valist, double)), c = 1;
-				break;
-			case "s":
-				str = va_arg(valist, char *), c = 1;
-				if (!str)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
+			}
+			printf("%s", str);
+			break;
 		} i++;
 	}
 	printf("\n"), va_end(valist);
