ade75467d5f0c4f7c5675c98bc2ef6b1b1e29998
Adeyemi Adunola <adunola.adeyemi@gmail.com>
2022-10-27 14:43:06 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * On error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
